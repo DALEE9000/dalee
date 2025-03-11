721a68f2eb2c52c6bf31cfc7cdc5e0aedf7cfd29
DALEE9000 <david.a.lee95@gmail.com>
2025-03-10 22:07:33 -0400

    updating with splash page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 items-center text-center">
 
         <div className="flex flex-col sm:flex-row gap-4 items-center">
-          <p>Welcome to David's website</p>
+          <p>Welcome to David&apos;s website</p>
         </div>
       </main>
       <footer className="mt-8 flex gap-6 flex-wrap items-center justify-center">
