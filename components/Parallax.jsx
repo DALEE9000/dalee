3d68560a49ff095cbdb42c82e9417498f4e2317a
DALEE9000 <david.a.lee95@gmail.com>
2025-03-12 04:22:47 -0400

    more refactoring

--- components/Parallax.jsx
+++ components/Parallax.jsx
@@ -1,8 +1,22 @@
 "use client"
 
-import Image from "next/image";
 import { motion } from 'framer-motion';
 import styles from "./Parallax.module.css";
+
+{/* This ensures a seamless horizontal wrap */}
+const animateparam = [
+  { initialX: '100%', animateX: '-100%' },
+  { initialX: '0%', animateX: '-200%' },
+  { initialX: '200%', animateX: '0%' },
+];
+
+{/* Add dynamic layers here */}
+const elements = [
+  { element: "cloud1", duration: 120 },
+  { element: "cloud2", duration: 90 },
+  { element: "cloud3", duration: 150 },
+  { element: "mountains", duration: 240 }
+]
 
 export default function Parallax(
     children,
@@ -12,208 +26,38 @@ export default function Parallax(
         className={styles['parallax-container']}
     >
       {/* STATIC LAYERS*/}
-
-      <div
-        className={`${styles['static-layer']}`}
-        style={{ backgroundImage: 'url(/pixelart/sky1.png)' }} // Adjust image paths
-      />
-
-      <div
-        className={`${styles['static-layer']}`}
-        style={{ backgroundImage: 'url(/pixelart/sky2.png)' }} // Adjust image paths
-      />
-
-      <div
-        className={`${styles['static-layer']}`}
-        style={{ backgroundImage: 'url(/pixelart/sky3.png)' }} // Adjust image paths
-      />
-
-      <div
-        className={`${styles['static-layer']}`}
-        style={{ backgroundImage: 'url(/pixelart/sun1.png)' }} // Adjust image paths
-      />
+      {["sky1", "sky2", "sky3", "sun1"].map((bg) => (
+        <div
+          key={bg}
+          className={`${styles['static-layer']}`}
+          style={{ backgroundImage: `url(/pixelart/${bg}.png)` }}
+        />
+      ))}
 
       {/*DYNAMIC LAYERS*/}
       {/* You need to triple each motion.div so that the animation loops correctly. */}
 
-      {/* CLOUD1 */}
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['cloud1']}`}
-        style={{ backgroundImage: 'url(/pixelart/cloud1.png)' }}
-        initial={{ x: '100%' }}
-        animate={{ x: '-100%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 120, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/cloud1.png)' }} />
-      </motion.div>
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['cloud1']}`}
-        style={{ backgroundImage: 'url(/pixelart/cloud1.png)' }}
-        initial={{ x: '0%' }}
-        animate={{ x: '-200%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 120, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/cloud1.png)' }} />
-      </motion.div>
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['cloud1']}`}
-        style={{ backgroundImage: 'url(/pixelart/cloud1.png)' }}
-        initial={{ x: '200%' }}
-        animate={{ x: '0%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 120, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/cloud1.png)' }} />
-      </motion.div>
-
-      {/* CLOUD2 */}
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['cloud2']}`}
-        style={{ backgroundImage: 'url(/pixelart/cloud2.png)' }}
-        initial={{ x: '100%' }}
-        animate={{ x: '-100%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 90, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/cloud2.png)' }} />
-      </motion.div>
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['cloud2']}`}
-        style={{ backgroundImage: 'url(/pixelart/cloud2.png)' }}
-        initial={{ x: '0%' }}
-        animate={{ x: '-200%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 90, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/cloud2.png)' }} />
-      </motion.div>
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['cloud2']}`}
-        style={{ backgroundImage: 'url(/pixelart/cloud2.png)' }}
-        initial={{ x: '200%' }}
-        animate={{ x: '0%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 90, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/cloud2.png)' }} />
-      </motion.div>
-
-      {/* CLOUD3 */}
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['cloud3']}`}
-        style={{ backgroundImage: 'url(/pixelart/cloud3.png)' }}
-        initial={{ x: '100%' }}
-        animate={{ x: '-100%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 150, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/cloud3.png)' }} />
-      </motion.div>
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['cloud3']}`}
-        style={{ backgroundImage: 'url(/pixelart/cloud3.png)' }}
-        initial={{ x: '0%' }}
-        animate={{ x: '-200%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 150, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/cloud3.png)' }} />
-      </motion.div>
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['cloud3']}`}
-        style={{ backgroundImage: 'url(/pixelart/cloud3.png)' }}
-        initial={{ x: '200%' }}
-        animate={{ x: '0%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 150, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/cloud3.png)' }} />
-      </motion.div>
-
-      {/* MOUNTAIN */}
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['mountain']}`}
-        style={{ backgroundImage: 'url(/pixelart/mountains.png)' }}
-        initial={{ x: '100%' }}
-        animate={{ x: '-100%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 240, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/mountains.png)' }} />
-      </motion.div>
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['mountain']}`}
-        style={{ backgroundImage: 'url(/pixelart/mountains.png)' }}
-        initial={{ x: '0%' }}
-        animate={{ x: '-200%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 240, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/mountains.png)' }} />
-      </motion.div>
-
-      <motion.div
-        className={`${styles['parallax-layer']} ${styles['mountain']}`}
-        style={{ backgroundImage: 'url(/pixelart/mountains.png)' }}
-        initial={{ x: '200%' }}
-        animate={{ x: '0%' }}
-        transition={{
-          repeat: Infinity,
-          duration: 240, // Different speed for each layer
-          ease: 'linear',
-        }}
-      >
-        <div className={styles['parallax-layer']} style={{ backgroundImage: 'url(/pixelart/mountains.png)' }} />
-      </motion.div>
-
-
-
+      {elements.map(({ element, duration }) =>
+        animateparam.map((layer, index) => (
+          <motion.div
+            key={`${element}-${index}`}
+            className={`${styles["parallax-layer"]} ${styles[element]}`}
+            style={{ backgroundImage: `url(/pixelart/${element}.png)` }}
+            initial={{ x: layer.initialX }}
+            animate={{ x: layer.animateX }}
+            transition={{
+              repeat: Infinity,
+              duration,
+              ease: "linear",
+            }}
+          >
+            <div
+              className={styles["parallax-layer"]}
+              style={{ backgroundImage: `url(/pixelart/${element}.png)` }}
+            />
+          </motion.div>
+        ))
+      )}
     </div>
   );
 }
