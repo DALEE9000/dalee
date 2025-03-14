61a9a50481ca2cc149194d9c73d99ba47d2ae176
DALEE9000 <david.a.lee95@gmail.com>
2025-03-14 15:21:26 -0400

    changing viewport res

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
     <head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <title>David A. Lee</title>
     </head>
       <body
         className={`${jersey.variable} antialiased ${isLoading ? "splash-active" : ""}`}
--- components/Buttons.jsx
+++ components/Buttons.jsx
@@ -23,7 +23,7 @@ export default function SplashButton ( { finishLoading }) {
             <>
             {isButtonVisible && 
             <button 
-                onClick={() => { finishLoading(); handleMouseEnter(); }}
+                onClick={() => finishLoading()}
                 className="fixed right-0 bottom-0 p-0 bg-transparent border-none outline-none cursor-pointer"
                 style={{
                     all: 'unset', // Remove all default button styles (including margin, padding, etc.)
