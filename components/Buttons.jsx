e516f2261128634973e34f4e013724899383dd9c
DALEE9000 <david.a.lee95@gmail.com>
2025-05-02 17:32:46 -0400

    updates

--- components/Buttons.jsx
+++ components/Buttons.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import styles from './splash/Splash.module.css';
 
-export function SplashButton ( { setOnSplash, setIsButtonLoading }) {
+export default function SplashButton ( { setOnSplash, setIsButtonLoading }) {
     const [imageSrc, setImageSrc] = useState("/pixelart/button1.png");
 
     function handleMouseEnter() {
