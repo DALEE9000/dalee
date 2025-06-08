e4826ead16db4d62c359fedc2210dc4adcbd9401
DALEE9000 <david.a.lee95@gmail.com>
2025-06-07 20:37:42 -0400

    weather

--- components/Weather.js
+++ components/Weather.js
@@ -7,13 +7,13 @@ export async function getWeather() {
     const geoRes = await fetch('/api/geolocate');
     if (geoRes.ok) {
       geoData = await geoRes.json();
-      lat = geoData.latitude;
-      lon = geoData.longitude;
-      city = geoData.city;
-      country = geoData.country;
+      lat = geoData.geo.latitude;
+      lon = geoData.geo.longitude;
+      city = geoData.geo.city;
+      country = geoData.geo.country;
+      zip = geoData.geo.postalCode
       ip = geoData.ip;
 
-      console.log(geoData)
       console.log("Vercel Geolocation:", lat, lon, city, country, ip);
     } else {
       console.warn("Vercel Geolocation failed with status:", geoRes.status);
@@ -21,12 +21,24 @@ export async function getWeather() {
 
     // STEP 2: Construct weather API URL
     let weatherUrl = `/api/weather`;
-    if (lat && lon) {
+
+    if (country === 'US' || country === 'CA') {
+      if (geoData.postal_code) {
+        weatherUrl += `?zip=${geoData.postal_code}`;
+        console.log("Using ZIP:", geoData.postal_code);
+      } else if (lat && lon) {
+        // fallback to lat/lon if zip is missing
+        weatherUrl += `?lat=${lat}&lon=${lon}`;
+        console.warn("ZIP not found, falling back to lat/lon");
+      } else {
+        console.warn("No location data, falling back to auto:ip");
+      }
+    } else if (lat && lon) {
       weatherUrl += `?lat=${lat}&lon=${lon}`;
-      console.log('weaterurl', weatherUrl)
+      console.log("Using lat/lon:", lat, lon);
     } else {
-      console.warn("Falling back to auto:ip");
-      // Leave URL as default
+      console.warn("No location data, falling back to auto:ip");
+      // Leave weatherUrl unchanged for IP fallback
     }
 
     // STEP 3: Fetch weather
